9e0fdcad954b2f0a6b92c806628344b6d9e326f7
Anirudh-G217 <53647649+Anirudh-G217@users.noreply.github.com>
2021-09-24 17:13:09 +0530

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,87 @@
+import datetime
+import hashlib
+class Block:
+    data = None
+    previousHash = None
+    currentHash = None
+    timeStamp = None
+    nextBlock = None
+    blockNumber = 0
+    nonce = 0
+
+    def __init__(self,data):
+        self.data = data
+        self.timeStamp = datetime.datetime.now()
+
+    def hash(self):
+        h = hashlib.sha256()
+        h.update(str(self.nonce).encode('utf-8') + str(self.data).encode('utf-8') + str(self.previousHash).encode('utf-8') + str(self.timeStamp).encode('utf-8') + str(self.blockNumber).encode('utf-8'))
+        return h.hexdigest()
+    
+    def __str__(self):
+        return "Block Hash: " + str(self.hash()) + "\nBlockNo: " + str(self.blockNumber) + "\nBlock Data: " + str(self.data) + "\nHashes: " + str(self.nonce) + "\nTimeStamp: " + str(self.timeStamp) +"\n--------------"      
+
+class Blockchain:
+
+    diff = 20
+    maxNonce = 2**32
+    target = 2 ** (256-diff)
+    block = Block("Genesis")
+    dummy = head = block
+
+    def add(self, block):
+
+        block.previousHash = self.block.hash()
+        block.blockNumber = self.block.blockNumber + 1
+
+        self.block.nextBlock = block
+        self.block = self.block.nextBlock
+
+    def mine(self, block):
+        for n in range(self.maxNonce):
+            if int(block.hash(), 16) <= self.target:
+                self.add(block)
+                print(block)
+                break
+            else:
+                block.nonce += 1
+
+class TransactionQueue:
+
+    block = Block("Genesis")
+    dummy = head = block
+
+    def add(self, block):
+
+        self.block.nextBlock = block
+        self.block = self.block.nextBlock
+
+blockchain = Blockchain()
+verified_pool = Blockchain()
+unverified_pool = TransactionQueue()
+choice=-1
+
+while(choice!=0):
+    print("Enter choice:\n1.Add transactionn\n2.Show transaction\n3.Exit")
+    choice = int(input())
+    if(choice == 1):
+        amount = input("Enter amount: ")
+        unverified_pool.add(Block(amount))
+    elif(choice == 2):
+        '''while blockchain.head != None:
+            print(blockchain.head)
+            blockchain.head = blockchain.head.nextBlock
+        blockchain.head = blockchain.dummy
+        '''
+        while unverified_pool.head!=None:
+            print(unverified_pool.head)
+            unverified_pool.head = unverified_pool.head.nextBlock
+        unverified_pool.head = unverified_pool.dummy
+
+    elif(choice == 3):
+        break
+    else:
+        print("Incorrect choice")
+
+
+    
