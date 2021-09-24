602a20ac2497a6ee595677b822e8b8ef1eb0b747
Anirudh-G217 <53647649+Anirudh-G217@users.noreply.github.com>
2021-09-24 19:23:43 +0530

    Add files via upload

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,7 @@
 import datetime
 import hashlib
+import threading
+
 class Block:
     data = None
     previousHash = None
@@ -9,9 +11,9 @@ class Block:
     blockNumber = 0
     nonce = 0
 
-    def __init__(self,data):
+    def __init__(self,data,timeStamp):
         self.data = data
-        self.timeStamp = datetime.datetime.now()
+        self.timeStamp = timeStamp
 
     def hash(self):
         h = hashlib.sha256()
@@ -26,7 +28,7 @@ class Blockchain:
     diff = 20
     maxNonce = 2**32
     target = 2 ** (256-diff)
-    block = Block("Genesis")
+    block = Block("Genesis",datetime.datetime.now())
     dummy = head = block
 
     def add(self, block):
@@ -37,18 +39,19 @@ class Blockchain:
         self.block.nextBlock = block
         self.block = self.block.nextBlock
 
+
     def mine(self, block):
         for n in range(self.maxNonce):
             if int(block.hash(), 16) <= self.target:
                 self.add(block)
-                print(block)
+                print("Block " + str(block.blockNumber) + " added to verified pool in background")
                 break
             else:
                 block.nonce += 1
 
 class TransactionQueue:
 
-    block = Block("Genesis")
+    block = Block("Q",datetime.datetime.now())
     dummy = head = block
 
     def add(self, block):
@@ -56,32 +59,53 @@ class TransactionQueue:
         self.block.nextBlock = block
         self.block = self.block.nextBlock
 
-blockchain = Blockchain()
 verified_pool = Blockchain()
 unverified_pool = TransactionQueue()
 choice=-1
+done=0
+
+def verifyBlock():
+    global unverified_pool
+    global verified_pool
+    global done
+    while(done==0):
+        while(unverified_pool.head!=None and done==0):
+            
+            if(unverified_pool.head!=unverified_pool.dummy):
+                verified_pool.mine(Block(unverified_pool.head.data,unverified_pool.head.timeStamp))
+                while(unverified_pool.head == unverified_pool.block and done==0):
+                    pass
+            if(unverified_pool.head.nextBlock!=None):
+                unverified_pool.head = unverified_pool.head.nextBlock
+
+t1 = threading.Thread(target = verifyBlock, args = ())
+t1.start()
 
 while(choice!=0):
-    print("Enter choice:\n1.Add transactionn\n2.Show transaction\n3.Exit")
+
+    print("Enter choice:\n1.Add transaction\n2.Show verified transaction\n3.Exit")
     choice = int(input())
+
     if(choice == 1):
+
         amount = input("Enter amount: ")
-        unverified_pool.add(Block(amount))
+        unverified_pool.add(Block(amount,datetime.datetime.now()))
+
     elif(choice == 2):
-        '''while blockchain.head != None:
-            print(blockchain.head)
-            blockchain.head = blockchain.head.nextBlock
-        blockchain.head = blockchain.dummy
-        '''
-        while unverified_pool.head!=None:
-            print(unverified_pool.head)
-            unverified_pool.head = unverified_pool.head.nextBlock
-        unverified_pool.head = unverified_pool.dummy
+
+        start = verified_pool.dummy
+        while start!=None:
+            print(start)
+            start = start.nextBlock
 
     elif(choice == 3):
+
         break
+
     else:
+
         print("Incorrect choice")
 
-
+done = 1
+t1.join()
     
